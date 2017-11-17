92d18f5fc7d58d367e9eb266ee8f65587cb64d17
Shamane Siriwardana <shamane92siriwardhana@gmail.com>
2017-11-17 19:54:30 +0800

    Add files via upload

--- ntm/ntm_cell.py
+++ ntm/ntm_cell.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 import numpy as np
 
-class NTMCell():
+class NTMCell():  #this is the class for the NTM cell . Same as LSTM cell but with external memory as in the paper 
     def __init__(self, rnn_size, memory_size, memory_vector_dim, read_head_num, write_head_num,
                  addressing_mode='content_and_loaction', shift_range=1, reuse=False, output_dim=None):
-        self.rnn_size = rnn_size
-        self.memory_size = memory_size
+        self.rnn_size = rnn_size #hidden dimentions of the RNN cell 
+        self.memory_size = memory_size  #internal size of a memory vector 
         self.memory_vector_dim = memory_vector_dim
-        self.read_head_num = read_head_num
+        self.read_head_num = read_head_num  #how many heads 
         self.write_head_num = write_head_num
         self.addressing_mode = addressing_mode
         self.reuse = reuse
-        self.controller = tf.nn.rnn_cell.BasicRNNCell(self.rnn_size)
+        self.controller = tf.nn.rnn_cell.BasicRNNCell(self.rnn_size)  #this is the controller network 
         self.step = 0
         self.output_dim = output_dim
         self.shift_range = shift_range
@@ -20,11 +20,12 @@ class NTMCell():
         prev_read_vector_list = prev_state['read_vector_list']      # read vector in Sec 3.1 (the content that is
                                                                     # read out, length = memory_vector_dim)
         prev_controller_state = prev_state['controller_state']      # state of controller (LSTM hidden state)
+                                                                    #can take as the controller state RNN 
 
         # x + prev_read_vector -> controller (RNN) -> controller_output
         controller_input = tf.concat([x] + prev_read_vector_list, axis=1)
         with tf.variable_scope('controller', reuse=self.reuse):
-            controller_output, controller_state = self.controller(controller_input, prev_controller_state)
+            controller_output, controller_state = self.controller(controller_input, prev_controller_state) #send through the lstm
 
         # controller_output     -> k (dim = memory_vector_dim, compared to each vector in M, Sec 3.1)
         #                       -> beta (positive scalar, key strength, Sec 3.1)                -> w^c
@@ -34,25 +35,25 @@ class NTMCell():
         #                       -> gamma (scalar (>= 1), sharpen the final result, Sec 3.2)     -> w    * num_heads
         # controller_output     -> erase, add vector (dim = memory_vector_dim, \in (0, 1), Sec 3.2)     * write_head_num
 
-        num_parameters_per_head = self.memory_vector_dim + 1 + 1 + (self.shift_range * 2 + 1) + 1
-        num_heads = self.read_head_num + self.write_head_num
+        num_parameters_per_head = self.memory_vector_dim + 1 + 1 + (self.shift_range * 2 + 1) + 1 #here kt means 20 dim thing 
+        num_heads = self.read_head_num + self.write_head_num #two heads should predict these things 
         total_parameter_num = num_parameters_per_head * num_heads + self.memory_vector_dim * 2 * self.write_head_num
         with tf.variable_scope("o2p", reuse=(self.step > 0) or self.reuse):
-            o2p_w = tf.get_variable('o2p_w', [controller_output.get_shape()[1], total_parameter_num],
+            o2p_w = tf.get_variable('o2p_w', [controller_output.get_shape()[1], total_parameter_num], #prediction layer weight
                                     initializer=tf.random_normal_initializer(mean=0.0, stddev=0.5))
             o2p_b = tf.get_variable('o2p_b', [total_parameter_num],
                                     initializer=tf.random_normal_initializer(mean=0.0, stddev=0.5))
-            parameters = tf.nn.xw_plus_b(controller_output, o2p_w, o2p_b)
-        head_parameter_list = tf.split(parameters[:, :num_parameters_per_head * num_heads], num_heads, axis=1)
-        erase_add_list = tf.split(parameters[:, num_parameters_per_head * num_heads:], 2 * self.write_head_num, axis=1)
+            parameters = tf.nn.xw_plus_b(controller_output, o2p_w, o2p_b) #computer the output multiply by weights and add bias
+        head_parameter_list = tf.split(parameters[:, :num_parameters_per_head * num_heads], num_heads, axis=1) #spliting parameters 
+        erase_add_list = tf.split(parameters[:, num_parameters_per_head * num_heads:], 2 * self.write_head_num, axis=1) #these are earase and add when used in reading and writing 
 
         # k, beta, g, s, gamma -> w
 
-        prev_w_list = prev_state['w_list']  # vector of weightings (blurred address) over locations
-        prev_M = prev_state['M']
-        w_list = []
+        prev_w_list = prev_state['w_list']  # vector of weightings (blurred address) initialized softmax  scores for each memory location we use this for the interpolation 
+        prev_M = prev_state['M'] #previous memory matrix 
+        w_list = [] 
         p_list = []
-        for i, head_parameter in enumerate(head_parameter_list):
+        for i, head_parameter in enumerate(head_parameter_list): #read right head weights 
 
             # Some functions to constrain the result in specific range
             # exp(x)                -> x > 0
@@ -60,17 +61,17 @@ class NTMCell():
             # softmax(x)            -> sum_i x_i = 1
             # log(exp(x) + 1) + 1   -> x > 1
 
-            k = tf.tanh(head_parameter[:, 0:self.memory_vector_dim])
-            beta = tf.sigmoid(head_parameter[:, self.memory_vector_dim]) * 10        # do not use exp, it will explode!
-            g = tf.sigmoid(head_parameter[:, self.memory_vector_dim + 1])
-            s = tf.nn.softmax(
+            k = tf.tanh(head_parameter[:, 0:self.memory_vector_dim]) #getting the K values  inoder to calculate the content based similarity 
+            beta = tf.sigmoid(head_parameter[:, self.memory_vector_dim]) * 10    # do not use exp, it will explode!
+            g = tf.sigmoid(head_parameter[:, self.memory_vector_dim + 1])  #this is the interpolation gate 
+            s = tf.nn.softmax(       #these parameters are for the shift and direct access 
                 head_parameter[:, self.memory_vector_dim + 2:self.memory_vector_dim + 2 + (self.shift_range * 2 + 1)]
             )
-            gamma = tf.log(tf.exp(head_parameter[:, -1]) + 1) + 1
+            gamma = tf.log(tf.exp(head_parameter[:, -1]) + 1) + 1      #this is for the shaprnenning 
             with tf.variable_scope('addressing_head_%d' % i):
-                w = self.addressing(k, beta, g, s, gamma, prev_M, prev_w_list[i])     # Figure 2
-            w_list.append(w)
-            p_list.append({'k': k, 'beta': beta, 'g': g, 's': s, 'gamma': gamma})
+                w = self.addressing(k, beta, g, s, gamma, prev_M, prev_w_list[i])   # Figure 2 This complte the adressing and update the weights 
+            w_list.append(w)  #for each read and write head we need this 
+            p_list.append({'k': k, 'beta': beta, 'g': g, 's': s, 'gamma': gamma})    #ths is the parameter list 
 
         # Reading (Sec 3.1)
 
@@ -120,27 +121,27 @@ class NTMCell():
 
         # Cosine Similarity
 
-        k = tf.expand_dims(k, axis=2)
-        inner_product = tf.matmul(prev_M, k)
+        k = tf.expand_dims(k, axis=2)  #this is to get the values of the vector 
+        inner_product = tf.matmul(prev_M, k)  #get unner prodct with the each memory locations there can be 128 memory locations 
         k_norm = tf.sqrt(tf.reduce_sum(tf.square(k), axis=1, keep_dims=True))
         M_norm = tf.sqrt(tf.reduce_sum(tf.square(prev_M), axis=2, keep_dims=True))
         norm_product = M_norm * k_norm
-        K = tf.squeeze(inner_product / (norm_product + 1e-8))                   # eq (6)
+        K = tf.squeeze(inner_product / (norm_product + 1e-8)) #get the cosine similarity                # eq (6)
 
         # Calculating w^c
 
-        K_amplified = tf.exp(tf.expand_dims(beta, axis=1) * K)
-        w_c = K_amplified / tf.reduce_sum(K_amplified, axis=1, keep_dims=True)  # eq (5)
+        K_amplified = tf.exp(tf.expand_dims(beta, axis=1) * K) #
+        w_c = K_amplified / tf.reduce_sum(K_amplified, axis=1, keep_dims=True)  # eq (5) get the normalize things 
 
-        if self.addressing_mode == 'content':                                   # Only focus on content
+        if self.addressing_mode == 'content':         # Only focus on content #if adressing mode is only content 
             return w_c
 
         # Sec 3.3.2 Focusing by Location
 
-        g = tf.expand_dims(g, axis=1)
-        w_g = g * w_c + (1 - g) * prev_w                                        # eq (7)
+        g = tf.expand_dims(g, axis=1)  #get the interpolation gate 
+        w_g = g * w_c + (1 - g) * prev_w               # eq (7) element wise multiplication 
 
-        s = tf.concat([s[:, :self.shift_range + 1],
+        s = tf.concat([s[:, :self.shift_range + 1],  #this is circular convolution matrix 
                        tf.zeros([s.get_shape()[0], self.memory_size - (self.shift_range * 2 + 1)]),
                        s[:, -self.shift_range:]], axis=1)
         t = tf.concat([tf.reverse(s, axis=[1]), tf.reverse(s, axis=[1])], axis=1)
@@ -152,14 +153,14 @@ class NTMCell():
         w_sharpen = tf.pow(w_, tf.expand_dims(gamma, axis=1))
         w = w_sharpen / tf.reduce_sum(w_sharpen, axis=1, keep_dims=True)        # eq (9)
 
-        return w
+        return w #getting the location and content based adressing weight for each memory location 
 
     def zero_state(self, batch_size, dtype):
         def expand(x, dim, N):
             return tf.concat([tf.expand_dims(x, dim) for _ in range(N)], axis=dim)
 
         with tf.variable_scope('init', reuse=self.reuse):
-            state = {
+            state = {            #state is a tupple 
                 # 'controller_state': self.controller.zero_state(batch_size, dtype),
                 # 'read_vector_list': [tf.zeros([batch_size, self.memory_vector_dim])
                 #                      for _ in range(self.read_head_num)],
@@ -169,11 +170,11 @@ class NTMCell():
                 'controller_state': expand(tf.tanh(tf.get_variable('init_state', self.rnn_size,
                                             initializer=tf.random_normal_initializer(mean=0.0, stddev=0.5))),
                                   dim=0, N=batch_size),
-                'read_vector_list': [expand(tf.nn.softmax(tf.get_variable('init_r_%d' % i, [self.memory_vector_dim],
+                'read_vector_list': [expand(tf.nn.softmax(tf.get_variable('init_r_%d' % i, [self.memory_vector_dim], #this is for the read vector weights
                                             initializer=tf.random_normal_initializer(mean=0.0, stddev=0.5))),
                                   dim=0, N=batch_size)
                            for i in range(self.read_head_num)],
-                'w_list': [expand(tf.nn.softmax(tf.get_variable('init_w_%d' % i, [self.memory_size],
+                'w_list': [expand(tf.nn.softmax(tf.get_variable('init_w_%d' % i, [self.memory_size],   #this is the weight vec list 
                                             initializer=tf.random_normal_initializer(mean=0.0, stddev=0.5))),
                                   dim=0, N=batch_size) if self.addressing_mode == 'content_and_loaction'
                            else tf.zeros([batch_size, self.memory_size])
