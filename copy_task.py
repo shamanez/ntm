28af6c6e0ff45e5b6b0c2f11c00e94a4f999d6b0
Shamane Siriwardana <shamane92siriwardhana@gmail.com>
2017-12-19 19:49:35 +0800

    Add files via upload

--- copy_task.py
+++ copy_task.py
@@ -10,11 +10,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', default="train")
     parser.add_argument('--restore_training', default=False)
-    parser.add_argument('--test_seq_length', type=int, default=2)
+    parser.add_argument('--test_seq_length', type=int, default=5)
     parser.add_argument('--model', default="NTM")
     parser.add_argument('--rnn_size', default=128)
     parser.add_argument('--rnn_num_layers', default=3)
-    parser.add_argument('--max_seq_length', default=2)
+    parser.add_argument('--max_seq_length', default=5)
     parser.add_argument('--memory_size', default=128) #this is how many memory locations 
     parser.add_argument('--memory_vector_dim', default=25)
     parser.add_argument('--batch_size', default=5)
@@ -22,7 +22,7 @@ def main():
     parser.add_argument('--shift_range', default=1)
     parser.add_argument('--num_epoches', default=1000000)
     parser.add_argument('--learning_rate', default=1e-4)
-    parser.add_argument('--save_dir', default='./save/copy_task')
+    parser.add_argument('--save_dir', default='/home/dl/Documents/shamane-research/One-shot-learning aproach/ntm-master/save')
     parser.add_argument('--tensorboard_dir', default='./summary/copy_task')
     args = parser.parse_args()
     if args.mode == 'train':
@@ -51,24 +51,24 @@ def train(args):
         train_writer = tf.summary.FileWriter(args.tensorboard_dir, sess.graph)
         plt.ion()
         plt.show()
-        for b in range(args.num_epoches):
+        for b in range(args.num_epoches): #training iteration 
             seq_length = np.random.randint(1, args.max_seq_length + 1)
-            model = model_list[seq_length - 1]
-            print("printing the seq leangth",seq_length)
+            model = model_list[seq_length - 1]  #same model which has made to different sequence lengths. 
+   
             # seq_length = args.max_seq_length
             x = generate_random_strings(args.batch_size, seq_length, args.vector_dim)
     
-            feed_dict = {model.x: x}
+            feed_dict = {model.x: x} #creating a feed dictionary, Element in one sequence is 8 bits.
             # print(sess.run([model.state_list, model.output_list], feed_dict=feed_dict))
-            if b % 100 == 0:        # test
+            if b % 100 == 0:        # test get the validation accuray while trainign 
                 p = 0               # select p th sample in the batch to show
-                print(x[p, :, :])
-                print(sess.run(model.o, feed_dict=feed_dict)[p, :, :])
+                print(x[p, :, :]) #input sequence
+                print(sess.run(model.o, feed_dict=feed_dict)[p, :, :]) #output sequence
                 state_list = sess.run(model.state_list, feed_dict=feed_dict)
                 if args.model == 'NTM':
                     w_plot = []
-                    M_plot = np.concatenate([state['M'][p, :, :] for state in state_list])
-                    for state in state_list:
+                    M_plot = np.concatenate([state['M'][p, :, :] for state in state_list]) #concatenate the memory 
+                    for state in state_list: #previous state list 
                         w_plot.append(np.concatenate([state['w_list'][0][p, :], state['w_list'][1][p, :]]))
                     plt.imshow(w_plot, interpolation='nearest', cmap='gray')
                     plt.draw()
@@ -78,17 +78,23 @@ def train(args):
                 train_writer.add_summary(merged_summary, b)
                 print('batches %d, loss %g' % (b, copy_loss))
             else:                   # train
-                sess.run(model.train_op, feed_dict=feed_dict)
+                sess.run(model.train_op, feed_dict=feed_dict) #gradient decent 
             if b % 5000 == 0 and b > 0:
                 saver.save(sess, args.save_dir + '/' + args.model + '/model.tfmodel', global_step=b)
 
 
 def test(args):
+    print("azzz")
     model = NTMCopyModel(args, args.test_seq_length)
     saver = tf.train.Saver()
-    ckpt = tf.train.get_checkpoint_state(args.save_dir)
+    ckpt = tf.train.get_checkpoint_state(args.save_dir+'/NTM')
+    print(args.save_dir)
+
     with tf.Session() as sess:
-        saver.restore(sess, ckpt.model_checkpoint_path)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            print("No Checkpoint found")
         x = generate_random_strings(args.batch_size, args.test_seq_length, args.vector_dim)
         feed_dict = {model.x: x}
         output, copy_loss, state_list = sess.run([model.o, model.copy_loss, model.state_list], feed_dict=feed_dict)
@@ -104,5 +110,5 @@ def test(args):
             plt.show()
 
 if __name__ == '__main__':
-    print("xsxsxsxs")
+
     main()
